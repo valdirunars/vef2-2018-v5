b81936104f715919e496692dcc02ddbe4eec2ae7
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2018-03-15 09:05:28 +0000

    Fyrsta útgáfa af verkefni 5

---
+++ src/components/home/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+
+import './Home.css';
+
+/* hér ætti að sækja forsíðu vefþjónustu til að sækja stats */
+
+export default class Home extends Component {
+
+  render() {
+
+    return (
+      <div className="home">
+        <p>útfæra</p>
+      </div>
+    );
+  }
+}
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import './Navigation.css';
+
+/* hér ætti að sækja gögn frá vefþjónustu fyrir valmynd */
+
+export default class Navigation extends Component {
+
+    return (
+      <nav className="navigation">
+        <p>útfæra</p>
+      </nav>
+    );
+  }
+}
